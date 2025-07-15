6d80cedfe00072decd75486dcb908a4019ab4279
Rebecca_is_alive_ <minehammer26@gmail.com>
2025-07-15 10:35:10 +0900

    adsf

--- hackquest/bof.py
+++ hackquest/bof.py
@@ -1,1 +1,10 @@
-asdf
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'hello world'
+
+if __name__ == '__main__':
+    app.run('127.0.0.1', 8080, debug=True)
